905dc65dde839fad1451113b6c029f3f4eb92f4c
lehoanggia2k <66256362+lehoanggia2k@users.noreply.github.com>
2023-12-07 18:54:24 +0300

    fistcommit
    fist commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to the Customer Management System!</h1>
+
+    <h2>View All Customers</h2>
+    <a href="{{ route('CustomerView') }}">View All Customers</a>
+
+</body>
+</html>
